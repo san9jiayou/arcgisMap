fa3035b19110bdfbcd663662d00eb3ae6f06ed4b
zhangjian <zj0187@woozoom.net>
2018-10-17 15:14:29 +0800

    add package

---
+++ maplibrary/src/main/java/com/woozoom/maplibrary/Test.java
@@ -0,0 +1,11 @@
+package com.woozoom.maplibrary;
+
+/**
+ * Created by zhangjian on 2018/10/17.
+ */
+
+public class Test {
+    public double add(double a, double b) {
+        return a + b;
+    }
+}
